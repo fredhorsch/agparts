073d141d6d953178c23d186ed79174826382c694
fredhorsch <frederic.horsch@code.berlin>
2022-03-16 16:07:18 +0100

    added List function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  //const frederic = ['eins', 'zwei'];
+
+  const [frederic, setFrederic] = useState(['eins', 'zwei']);
+
+  const sagHelloWorld = (valueText: string) => {
+    console.log("Hello World!" + valueText);
+  }
+
+  const pushElement = () => {
+    setFrederic([...frederic, "test"])
+    console.log(frederic);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    {frederic.map((value) =>{
+      return <button onClick={() => sagHelloWorld(value)}> {value} </button>
+    })}
+    <button onClick={() => pushElement()}> Fuege Hinzu  </button>
+  </>
   );
 }
 
