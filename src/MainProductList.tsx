642d0a2986e235d9bdc5391ccaff26d41671d841
fredhorsch <frederic.horsch@code.berlin>
2022-04-06 15:33:11 +0200

    Product List Rendering

--- src/MainProductList.tsx
+++ src/MainProductList.tsx
@@ -1,8 +1,20 @@
 const MainProductList = () => {
 
-    return (
-        <div>
+    const productList = [
+        {name: 'max', alter: '24'},
+        {name: 'frederic', alter: '100'}
+    ];
 
+    return (
+        <div className="productList">
+            {productList.map((element, index) => {
+                return (
+                    <div className="productListItem" id={`item${index}`}>
+                        <p>{element.name}</p>
+                        <p>{element.alter}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 }
