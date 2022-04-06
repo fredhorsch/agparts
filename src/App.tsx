dc3e4d78e1793cc7d654de7a27d68eea12ee6b41
fredhorsch <frederic.horsch@code.berlin>
2022-04-06 14:40:58 +0200

    Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 const App = () => {
 
   return (
   <>
+    <Header/>
   </>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <div className="topHeader">
+            <a href="https://www.ag-parts.de">Ag-Parts</a>
+        </div>
+    )
+}
+export default Header
