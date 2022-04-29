01f100aa0343977c062c4cf7a38b3d1fc9c499cd
fredhorsch <frederic.horsch@code.berlin>
2022-04-29 13:23:14 +0200

    Updated Files

--- src/Main.tsx
+++ src/Main.tsx
@@ -7,9 +7,9 @@ import './Main.scss'
 const Main = () => {
 
     const [productList, setProductList] = useState<ProductType[]>([
-        {name: 'max', price: 100, description: 'desc 1', img: './images/Terracut_01.jpeg', isClicked: false},
-        {name: 'fred', price: 200, description: 'desc 2', img: './images/FlexGrip_HM_80_01.jpg', isClicked: false},
-        {name: 'test', price: 300, description: 'desc 3', img: './images/Terracut_01.jpeg', isClicked: false},
+        {name: 'max', price: 53, description: 'desc 1', img: './images/FlexGrip_HM_80_01.jpeg', isClicked: false},
+        {name: 'fred', price: 38, description: 'desc 2', img: './images/Terracut_01.jpg', isClicked: false},
+        {name: 'test', price: 45, description: 'desc 3', img: './images/TerraCut_SW_02.jpeg', isClicked: false},
     ]);
 
     return (
